30044c0ab9d9c11c3f9f8390de424ad07e99fb79
Dhia <dhiabenfadhel@gmail.com>
2023-09-10 13:44:38 +0100

    Created Redux State slice for Getting Movies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
   selectUserName,
-  selectUserEmail,
+  /*selectUserEmail,*/
   selectUserPhoto,
   setUserLoginDetails,
   setSignOutState,
